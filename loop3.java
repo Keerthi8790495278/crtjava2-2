bc50d8a3db7b2f710bdc0261d8199f101252e646
Keerthi8790495278 <155128602+Keerthi8790495278@users.noreply.github.com>
2023-12-30 15:21:44 +0530

    Add files via upload

---
+++ loop3.java
@@ -0,0 +1,25 @@
+  import java.util.*;
+
+public class 9{
+	
+public static void main(String[] args) {
+	    
+Scanner s=new Scanner(System.in);
+	    
+int i=1,n;
+	    
+n=s.nextInt();
+	    
+while(i<=n)
+	    
+{
+	     	
+System.out.println(i);
+	     	
+i++;
+	    
+}
+	
+	
+}
+}
