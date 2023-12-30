dbfbe69de09ccd9fd46fcca44e6c8b8bca6a40bd
Keerthi8790495278 <155128602+Keerthi8790495278@users.noreply.github.com>
2023-12-30 12:36:02 +0530

    Add files via upload

---
+++ SwiMath.java
@@ -0,0 +1,135 @@
+ import java.util.*;
+
+
+ 
+public class SwitchMath
+{
+ 
+ 
+ 
+public static void main (String[]args)
+
+  {
+    
+ 
+Scanner s = new Scanner (System.in);
+  
+  
+ 
+int a, b;
+ 
+   
+ 
+System.out.println ("enter two numbers");
+
+    
+a = s.nextInt ();
+
+    
+b = s.nextInt ();
+
+    
+System.out.println ("1.add\n2.sub\n3.mul\n4.div");
+   
+ 
+ 
+ 
+System.out.println ("enter choice");
+ 
+   
+int c = s.nextInt ();
+
+switch (c)
+  
+{
+
+
+case 1:
+
+{
+	 
+System.out.println ("sum is" + (a + b));
+
+
+}
+
+
+break;
+
+
+case 2:
+
+{
+	  
+System.out.println ("difference is" + (a - b));
+	
+}
+	
+
+break;
+      
+
+case 3:
+	
+	
+{
+	  
+System.out.println ("product is" + (a * b));
+}
+	
+
+
+break;
+      
+
+case 4:
+
+	
+	
+{
+	  
+System.out.println ("division value is" + (a / b));
+
+
+}
+	
+
+break;
+      
+
+case 5:
+	
+	
+{
+	  S
+ystem.out.println (a % b);
+	
+
+}
+	
+
+break;
+      
+ 
+
+default:
+	
+	
+{
+	  
+System.out.println ("invalid")
+;
+	
+
+}
+      
+
+}
+  
+
+}
+
+
+}
+
+
