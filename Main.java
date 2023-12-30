b349d78491d2d837516e3c589b4d8d097c0239ad
Keerthi8790495278 <155128602+Keerthi8790495278@users.noreply.github.com>
2023-12-30 10:59:23 +0530

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,23 @@
+import java.util.*;
+public class Main
+{
+
+	public static void main(String[] args) {
+
+	    Scanner s=new Scanner(System.in);
+
+	    int a,b;
+	
+    a=s.nextInt();
+
+	    b=s.nextInt();
+	
+    int r=(a>b)?a:b;
+
+	    System.out.println(r);
+	
+}
+
+}
+
+
