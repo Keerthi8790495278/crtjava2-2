77a5093442d61ff962aa43188c123413b191f49e
Keerthi8790495278 <155128602+Keerthi8790495278@users.noreply.github.com>
2023-12-30 15:20:53 +0530

    Add files via upload

---
+++ loop1.java
@@ -0,0 +1,21 @@
+ public class loop1{
+
+public static void main(String[] args)
+ {
+	    
+int i=10;
+	    
+while(i>=1)
+	    
+{
+	        
+System.out.print(i+ " ");
+	        
+i--;
+	    
+}
+	    
+
+	
+}
+}
