9831b740e0734fb0104b020681b33ecebbe5a9ea
Keerthi8790495278 <155128602+Keerthi8790495278@users.noreply.github.com>
2023-12-30 11:09:22 +0530

    Add files via upload

---
+++ Smallest.java
@@ -0,0 +1,23 @@
+import java.util.*;
+
+public class Smallest
+{
+
+	public static void main(String[] args) {
+	
+    Scanner s=new Scanner(System.in);
+
+	    int a,b,c;
+
+	    a=s.nextInt();
+
+	    b=s.nextInt();
+
+	    c=s.nextInt();
+
+	    int r=(a<b && a<c)?a:(b<c)?b:c;
+
+	    System.out.println(r);
+
+	}
+}
