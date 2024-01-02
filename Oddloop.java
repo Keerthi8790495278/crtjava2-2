8416b412d72f6ed275799b4fa44c9c4abae245ef
Keerthi8790495278 <155128602+Keerthi8790495278@users.noreply.github.com>
2024-01-02 10:00:55 +0530

    Add files via upload

---
+++ Oddloop.java
@@ -0,0 +1,26 @@
+ import java.util.*;
+
+public class Oddloop{
+	
+public static void main(String[] args) {
+	    
+Scanner s=new Scanner(System.in);
+
+System.out.println("enter the value of n");	    
+int i=1,n;
+	    
+n=s.nextInt();
+	    
+while(i<=n)
+	    
+{
+	     	
+System.out.println(i);
+	     	
+i=i+2;
+	    
+}
+	
+	
+}
+}
