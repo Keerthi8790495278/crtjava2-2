25ea7eb46ccb0d5f2aab88c329e691123f77f3bf
Keerthi8790495278 <155128602+Keerthi8790495278@users.noreply.github.com>
2024-01-02 09:31:18 +0530

    Add files via upload

---
+++ For2.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+public class For2
+{
+	
+public static void main(String[] args) {
+	    
+Scanner s=new Scanner(System.in);
+	    
+int i,n;
+	    
+n=s.nextInt();
+	    
+for(i=2;i<=n;i=i+2)
+		
+{System.out.print(i+" ");
+	    
+}
+	
+}	
+
+}
