9ef5e2a35088a2c61653c594070f51bf1d6b3af4
Keerthi8790495278 <155128602+Keerthi8790495278@users.noreply.github.com>
2024-01-02 11:11:03 +0600

    Add files via upload

---
+++ Primes.java
@@ -0,0 +1,31 @@
+import java.util.Scanner;
+
+public class Prime
+{
+	
+public static void main(String[] args) {
+	    
+int i,n,c=0;
+	    
+Scanner s=new Scanner(System.in);
+	   
+ n=s.nextInt();
+	   
+ for(i=1;i<=n;i++)
+	    
+{if(n%i==0)
+	    
+c=c+1;
+	   
+ }
+	    
+{if(c==2)
+		
+System.out.println(n+"is a prime number");
+		
+else
+			
+System.out.println(n+"is a not prime number");
+	
+      }}
+}
