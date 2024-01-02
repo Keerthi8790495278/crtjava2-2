76f8dd8099fc3d3fda327fa2fc3ae5d35943afbd
Keerthi8790495278 <155128602+Keerthi8790495278@users.noreply.github.com>
2024-01-02 17:06:31 +0600

    Add files via upload

---
+++ Nestedalpha.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class Nestedalpha
+
+{
+	
+public static void main(String[] args) {
+	    
+int i,j;
+for(i=1;i<=5;i++)
+{
+for(j=1;j<=5;j++)
+
+    		
+{System.out.print((char)(64+j)+ " ");
+
+}
+System.out.println();
+			
+ }}}
+
