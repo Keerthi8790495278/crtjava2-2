733da9bf794546edd13da0b0cf7144fce192d78d
Keerthi8790495278 <155128602+Keerthi8790495278@users.noreply.github.com>
2024-01-02 15:40:05 +0600

    Add files via upload

---
+++ Series.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class Series
+{
+	
+public static void main(String[] args) {
+	    
+int i,n,n1=0 ;
+	    
+Scanner s=new Scanner(System.in);
+	   
+ n=s.nextInt();
+	   
+ for(i=1;i<=n;i++)
+	    
+{n1=n1+i;   
+ 
+	    
+System.out.print(n1+" ");
+}	
+    }
+}
