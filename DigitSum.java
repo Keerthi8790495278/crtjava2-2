f28e31ec627adeae72c4f8f3ad7c95b6d3456ec0
Keerthi8790495278 <155128602+Keerthi8790495278@users.noreply.github.com>
2024-01-02 10:01:27 +0530

    Add files via upload

---
+++ DigitSum.java
@@ -0,0 +1,63 @@
+import java.util.*;
+
+ 
+
+public class DigitSum{
+  
+ 
+
+public static void main (String[]args)
+  
+{
+    
+ 
+
+Scanner s = new Scanner (System.in);
+    
+ 
+
+System.out.println ("enter the value of n");
+    
+
+int n, temp, r, d = 0;
+    
+
+    
+ 
+
+n = s.nextInt ();
+temp = n;
+    
+ 
+
+while (n > 0)
+      
+ 
+      
+{
+	
+ 
+
+r = n % 10;
+	
+
+n = n / 10;
+	
+ 
+
+d = d + r;
+      
+
+}
+    
+ 
+
+System.out.println (d);
+  
+ 
+
+}
+
+}
+
+
