8340600cc0c30dac5664039dd48dc3f0e34dccac
Keerthi8790495278 <155128602+Keerthi8790495278@users.noreply.github.com>
2024-01-02 16:13:52 +0600

    Add files via upload

---
+++ Continue.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+public class Continue
+{
+	
+public static void main(String[] args) {
+	    
+int i,n,c=0;
+	    
+Scanner s=new Scanner(System.in);
+	   
+ n=s.nextInt();
+	   
+ for(i=1;i<=n;i++)
+	    
+{if(i%3==0)
+{	    
+continue;   
+ }
+	    		
+System.out.println(i);
+			
+ }}}
+
