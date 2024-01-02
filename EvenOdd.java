1564b48d54cd3d9d4631c38050a6bc09515e334b
Keerthi8790495278 <155128602+Keerthi8790495278@users.noreply.github.com>
2024-01-02 10:00:35 +0530

    Add files via upload

---
+++ EvenOdd.java
@@ -0,0 +1,97 @@
+import java.util.*;
+
+
+ 
+
+public class EvenOdd
+{
+  
+ 
+
+
+public static void main (String[]args)
+  
+
+{
+    
+ 
+
+
+Scanner s = new Scanner (System.in);
+    
+ 
+
+
+System.out.println ("enter the value of n");
+    
+
+
+int n, temp, r, d = 0,e=0;
+    
+
+    
+ 
+
+
+n = s.nextInt ();
+temp = n;
+    
+ 
+
+
+while (n > 0)
+      
+ 
+      
+
+{
+	
+ 
+
+
+r = n % 10;
+	
+
+
+n = n / 10;
+
+if(r%2==0)
+	
+ 
+
+
+{d = d + r;
+
+       
+
+
+}
+ 
+
+else
+{
+
+e=e+r;
+
+ 
+}}
+
+System.out.println (d);  
+ 
+
+System.out.println (e);
+   
+
+   
+ 
+
+
+}
+
+}
+
+
+
+
+
+ 
