91baaf876192d3a2dbb5a7e9ce6725a53c5553c1
Keerthi8790495278 <155128602+Keerthi8790495278@users.noreply.github.com>
2024-01-02 09:59:19 +0530

    Add files via upload

---
+++ Evenloop.java
@@ -0,0 +1,26 @@
+ import java.util.*;
+
+public class Evenloop{
+	
+public static void main(String[] args) {
+	    
+Scanner s=new Scanner(System.in);
+
+System.out.println("enter the value of n");	    
+int i=2,n;
+	    
+n=s.nextInt();
+	    
+while(i<=n)
+	    
+{
+	     	
+System.out.println(i);
+	     	
+i=i+2;
+	    
+}
+	
+	
+}
+}
