77290d1c53b67e3de9d682beaa14e0039b452453
Keerthi8790495278 <155128602+Keerthi8790495278@users.noreply.github.com>
2024-01-02 10:00:01 +0530

    Add files via upload

---
+++ Reverse.java
@@ -0,0 +1,111 @@
+import java.util.*;
+
+
+ 
+public class Reverse
+
+{
+  
+ 
+ 
+ 
+ 
+
+public static void main (String[]args)
+ 
+ 
+
+  {
+    
+ 
+ 
+ 
+ 
+
+Scanner s = new Scanner (System.in);
+    
+ 
+ 
+ 
+ 
+
+System.out.println ("enter the value of n");
+    
+ 
+ 
+ 
+
+int n, temp, r=0, d ;
+    
+ 
+ 
+ 
+ 
+ 
+ 
+
+n = s.nextInt ();
+    
+ 
+
+temp = n;
+    
+ 
+ 
+ 
+ 
+
+while (n > 0)
+      
+ 
+ 
+ 
+ 
+      
+{
+	
+ 
+ 
+ 
+ 
+
+d= n % 10;
+	
+ 
+ 
+ 
+
+n = n / 10;
+	
+ 
+
+r = r * 10 + d;
+      
+
+}
+    
+ 
+
+System.out.println (r);
+  
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+
+}
+
+ 
+}
+
+
+ 
+ 
+ 
+ 
+ 
