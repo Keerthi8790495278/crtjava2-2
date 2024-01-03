da088890410f7f0f30d6cdcb9ca5385f26c33354
Keerthi8790495278 <155128602+Keerthi8790495278@users.noreply.github.com>
2024-01-03 10:42:51 +0600

    Add files via upload

---
+++ Arrayfloat.java
@@ -0,0 +1,12 @@
+public class Arrayfloat{
+
+public static void main(String[] args) {
+	    
+float a[]={34.5f,87.6f,89.8f};
+int i;
+for(i=0;i<a.length;i++)
+		
+System.out.println(a[i]+ " ");
+	
+}
+}
