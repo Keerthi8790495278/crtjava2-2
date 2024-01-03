53828c3dda76ab1c05e47b7227ad1cd596c7b4ca
Keerthi8790495278 <155128602+Keerthi8790495278@users.noreply.github.com>
2024-01-03 15:50:09 +0530

    Add files via upload

---
+++ Diagsum.java
@@ -0,0 +1,28 @@
+import java.util.*;
+class  Diagsum{
+	public static void main(String[] args) 
+	{
+		Scanner s=new Scanner(System.in);
+		int m,n,i,j,a[][],sum=0;
+			System.out.println("enter num of rows and columns");
+			m=s.nextInt();
+			n=s.nextInt();
+			a=new int[m][n];
+		System.out.println("enter elements");
+
+		for(i=0;i<m;i++){
+			for(j=0;j<n;j++){
+	a[i][j]=s.nextInt();
+			}
+			}
+		System.out.println("matrix is");
+		for(i=0;i<m;i++){
+			for(j=0;j<n;j++){
+			    	if(i==j)
+			    	sum=sum+a[i][j];
+			}
+				
+	}
+	System.out.println(sum);
+}
+}
