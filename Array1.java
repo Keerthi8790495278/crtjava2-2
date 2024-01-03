8401bc96b255ec116660bd8f908b625e1cf93c3d
Keerthi8790495278 <155128602+Keerthi8790495278@users.noreply.github.com>
2024-01-03 12:14:42 +0600

    Add files via upload

---
+++ Array1.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+public class Array1      {
+
+public static void main(String[] args) {
+	Scanner s=new Scanner(System.in);
+int a[],i;
+a=new int[5];
+for(i=0;i<5;i++)
+	{		
+a[i]=s.nextInt();
+	}
+	for(i=0;i<5;i++)
+	{		
+	System.out.print(a[i]+ " ");
+}}
+}
