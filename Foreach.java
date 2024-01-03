010c119973680254febadc21e87660810acd8ff9
Keerthi8790495278 <155128602+Keerthi8790495278@users.noreply.github.com>
2024-01-03 10:42:35 +0600

    Add files via upload

---
+++ Foreach.java
@@ -0,0 +1,12 @@
+public class Foreach                                  {
+
+public static void main(String[] args) {
+	    
+float a[]={34.5f,87.6f,89.8f};
+int i;
+for(float x:a)
+	{		
+System.out.println(x+ " ");
+	}
+}
+}
