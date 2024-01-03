ae28ec4ae491ec12e614f8c5d5d7eca13c9e4c71
Keerthi8790495278 <155128602+Keerthi8790495278@users.noreply.github.com>
2024-01-03 12:15:15 +0600

    Add files via upload

---
+++ Array2.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+public class Array2      {
+
+public static void main(String[] args) {
+	Scanner s=new Scanner(System.in);
+int a[],i,n;
+n=s.nextInt();
+a=new int[n];
+
+for(i=0;i<n;i++)
+	{		
+a[i]=s.nextInt();
+	}
+	for(i=0;i<n;i++)
+	{		
+	System.out.print(a[i]+ " ");
+}}
+}
