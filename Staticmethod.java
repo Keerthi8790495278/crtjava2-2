952b689c93b0eb50890eda22b3dac2b05caf620b
Keerthi8790495278 <155128602+Keerthi8790495278@users.noreply.github.com>
2024-01-04 16:17:00 +0530

    Add files via upload

---
+++ Staticmethod.java
@@ -0,0 +1,22 @@
+class Staticmethod 
+{
+void m1()
+	{
+System.out.println("instance method");
+}
+static void m2(){
+System.out.println("static method");}
+
+	public static void main(String[] args) 
+	{
+	   // Staticmethod.m1();
+	    Staticmethod.m2();
+        Staticmethod  s1=new Staticmethodain();
+		
+		Staticmethod  s2=new Staticmethod ();
+	s1.m1();
+	s1.m2();
+	
+	}
+		
+}
