4cbbf1f742ac88dbfbc6be77af7c4ec1c49fa647
Keerthi8790495278 <155128602+Keerthi8790495278@users.noreply.github.com>
2024-01-04 12:39:50 +0530

    Add files via upload

---
+++ Object.java
@@ -0,0 +1,17 @@
+class Object 
+{int a,b;
+
+	public static void main(String[] args) 
+	{
+		Object s1=new Object();
+		s1.a=10;
+		s1.b=5;
+		Object s2=new Object();
+		s2.a=19;
+		s2.b=90;
+
+		System.out.println(s1.a+" "+s1.b);
+		System.out.println(s2.a+" "+s2.b);
+
+	}
+}
