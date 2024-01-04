c7f4b1f7eaf96bdc4568ea60b228b806af1ea44c
Keerthi8790495278 <155128602+Keerthi8790495278@users.noreply.github.com>
2024-01-04 12:39:32 +0530

    Add files via upload

---
+++ Instance1.java
@@ -0,0 +1,16 @@
+
+
+public class Instance1
+{int a;
+float b;
+boolean c;
+String d;
+
+	public static void main(String[] args) {
+	    Instance1 s1=new Instance1();
+	   System.out.println(s1.a);
+	   System.out.println(s1.b);
+	   System.out.println(s1.c);
+	   System.out.println(s1.d);
+	} 
+}
