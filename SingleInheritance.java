696b28685cb8dade62d20115ce53683388d6a26d
Keerthi8790495278 <155128602+Keerthi8790495278@users.noreply.github.com>
2024-01-05 12:38:31 +0530

    Add files via upload

---
+++ SingleInheritance.java
@@ -0,0 +1,26 @@
+class First
+{void m1(){
+	System.out.println("method 1");
+
+}
+void m2()
+{	System.out.println("method 2");
+
+}}
+class Second extends First
+{
+	void m3()
+		{
+	System.out.println("method 3");
+
+}
+
+}
+class SingleInheritance	
+{
+	public static void main(String args[])
+	{Second s1=new Second();
+	s1.m1();
+	s1.m2();
+	s1.m3();
+}}
