b3de6243efa65a690fdc8aa80f208164271ea4a1
Keerthi8790495278 <155128602+Keerthi8790495278@users.noreply.github.com>
2024-01-07 16:23:02 +0530

    Add files via upload

---
+++ Stringequal.java
@@ -0,0 +1,19 @@
+import java.io.*;
+import java.util.*;
+
+public class Stringequal {
+
+    public static void main(String[] args) {
+         String s1,s2;
+        Scanner s=new Scanner(System.in);
+      s1=s.next();
+        s2=s.next();
+        if(s1.equals(s2))
+        { System.out.println("Strings are same");}
+        else
+        {      System.out.println("Strings are not same");
+        }
+        
+        
+    }
+}
