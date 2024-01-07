582994ab7948106b559afbf8e0e42b26dfb8eafe
Keerthi8790495278 <155128602+Keerthi8790495278@users.noreply.github.com>
2024-01-07 16:22:49 +0530

    Add files via upload

---
+++ Stringconcatination.java
@@ -0,0 +1,11 @@
+import java.util.*;
+class Stringconcatination{
+    public static void main(String args[]){
+        Scanner s=new Scanner(System.in);
+        String s1,s2,s3;
+        s1=s.next();
+        s2=s.next();
+       
+        System.out.println(s1+" " +s2);
+    }
+}
